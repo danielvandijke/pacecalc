4f7f6ad965f3549aafc1ba8018c8900155c6816a
danielvandijke <76173762+danielvandijke@users.noreply.github.com>
2025-04-15 16:15:44 +0100

    Refactored pacecalc.cpp
    Moved more of the logic out of main and created a RunData class to encapsulate data relating to different units of distance.

--- pacecalc.cpp
+++ pacecalc.cpp
@@ -3,11 +3,9 @@
 
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <cstdlib>
-#include <format>
 #include <iomanip>
 #include <string>
+#include <sstream>
 
 constexpr float MILES_TO_KM = 1.60934f;
 constexpr float KM_TO_MILES = 0.621371f;
@@ -17,6 +15,10 @@ using namespace std;
 struct Time {
     int minutes;
     int seconds;
+
+    string to_string() const {
+        return std::to_string(minutes) + " minutes and " + std::to_string(seconds) + " seconds";
+    }
 };
 
 struct DistanceAndPace {
@@ -36,27 +38,30 @@ string get_date(){
             return date;
         }
     }
-    return "";
 }
 
 char get_distance_unit(){
     while (true){
         cout << "To select unit of distance type either 'm' or 'k':" << endl;
-        char input = getchar();
-        if (input == 'm'){
-            return 'm';
-        }
-        if (input == 'k'){
-            return 'k';
+        string input;
+        getline(cin, input);
+        if (input == "m" || input == "k") {
+            return input[0];
         }
         cout << "Invalid input" << endl;
     }
 }
 
 float get_distance_run(){
-    float distance;
+    string input;
+    float distance = 0.0f;
     cout << "Please enter distance run: " << endl;
-    cin >> distance;
+    getline(cin, input);
+    try {
+        distance = stof(input);
+    } catch (...) {
+        cout << "Invalid input. Defaulting to 0." << endl;
+    }
     return distance;
 }
 
@@ -67,67 +72,79 @@ string get_run_time_input(){
     return run_time;
 }
 
-void string_to_minutes_and_seconds(const string& run_time, int& mins, int& secs){
+Time parse_run_time_input(const string& run_time){
+    Time t{0, 0};
     size_t pos = run_time.find(":");
     if (pos != string::npos){
-        mins = stoi(run_time.substr(0, pos));
-        secs = stoi(run_time.substr(pos + 1));
+        t.minutes = stoi(run_time.substr(0, pos));
+        t.seconds = stoi(run_time.substr(pos + 1));
     }
+    return t;
 }
 
-float calculate_time_run(int mins, int secs){
+float time_in_decimal(int mins, int secs){
     return (float)mins + ((float)secs / 60);
 }
 
-float calculate_pace(Time time_run, float dist){
-    return calculate_time_run(time_run.minutes, time_run.seconds) / dist;
+float calculate_speed(Time time, float dist){
+    return time_in_decimal(time.minutes, time.seconds) / dist;
 }
 
-void float_to_minutes_and_seconds(const float& pace, int& pace_mins, int& pace_secs){
-    pace_mins = (int)pace;
-    float remainder = (pace - pace_mins) * 60;
-    pace_secs = (int)remainder;
+Time speed_to_pace(const float& speed){
+    Time pace = {};
+    pace.minutes = static_cast<int>(speed);
+    float remainder = (speed - static_cast<float>(pace.minutes)) * 60;
+    pace.seconds = static_cast<int>(remainder);
+    return pace;
 }
 
-void convert_distance_and_pace(char distance_unit, float input_distance, float pace, Time input_pace, DistanceAndPace& km_result, DistanceAndPace& mile_result){
-    if (distance_unit == 'k'){
-        km_result.distance = input_distance;
-        km_result.pace.minutes = input_pace.minutes;
-        km_result.pace.seconds = input_pace.seconds;
-        mile_result.distance = KM_TO_MILES * input_distance;
-        float mile_pace = MILES_TO_KM * pace;
-        float_to_minutes_and_seconds(mile_pace, mile_result.pace.minutes, mile_result.pace.seconds);
-    }
-    else {
-        mile_result.distance = input_distance;
-        mile_result.pace.minutes = input_pace.minutes;
-        mile_result.pace.seconds = input_pace.seconds;
-        km_result.distance = MILES_TO_KM * input_distance;
-        float km_pace = KM_TO_MILES * pace;
-        float_to_minutes_and_seconds(km_pace, km_result.pace.minutes, km_result.pace.seconds);
-    }
+Time calculate_pace(Time run_time, float dist){
+    float speed = calculate_speed(run_time, dist);
+    Time pace = speed_to_pace(speed);
+    return pace;
 }
 
-void print_results(string date, Time time_run, DistanceAndPace& km_data, DistanceAndPace& mile_data){
-    cout << "\nOn " << date << ",\nyou ran " << fixed << setprecision(2) << km_data.distance << " kilometers or " << fixed << setprecision(2) << mile_data.distance << " miles in a time of " << time_run.minutes << " minutes and " << time_run.seconds << " seconds at a pace of " 
-    << km_data.pace.minutes << " minutes and " << km_data.pace.seconds << " seconds per kilometer or " << mile_data.pace.minutes << " minutes and " << mile_data.pace.seconds << " seconds per mile." << endl;
+class RunData {
+    Time run_duration;
+    DistanceAndPace km_data;
+    DistanceAndPace mile_data;
+
+    public:
+        RunData(float distance, char distance_unit, Time run_duration)
+        : run_duration(run_duration) {
+            if (distance_unit == 'm'){
+               distance *= MILES_TO_KM;
+            }
+            km_data.distance = distance;
+            km_data.pace = calculate_pace(run_duration, km_data.distance);
+            mile_data.distance = distance * KM_TO_MILES;
+            mile_data.pace = calculate_pace(run_duration, mile_data.distance);
+        }
+        const DistanceAndPace& getKmData() const{ return km_data; }
+        const DistanceAndPace& getMileData() const{ return mile_data; }
+
+        string to_string() const {
+            stringstream result;
+            result << "you ran " << fixed << setprecision(2) << km_data.distance << " kilometers or " 
+            << fixed << setprecision(2) << mile_data.distance << " miles " 
+            << "in a time of " << run_duration.to_string() 
+            << " at a pace of " << km_data.pace.to_string() << " per kilometer or " 
+            << mile_data.pace.to_string() << " per mile.";
+            return result.str();
+        }
+};
+
+void print_results(string date, RunData& run_data){
+    cout << "\nOn " << date << ", " << run_data.to_string();
 }
 
-int main(int argc, char **argv)
+int main()
 {
     string date = get_date();
     char distance_unit = get_distance_unit();
-    float dist = get_distance_run();
+    float distance = get_distance_run();
     string run_time_input = get_run_time_input();
-    Time time_run{0, 0};
-    string_to_minutes_and_seconds(run_time_input, time_run.minutes, time_run.seconds);
-    float pace = calculate_pace(time_run, dist);
-    int pace_mins = 0, pace_secs = 0;
-    float_to_minutes_and_seconds(pace, pace_mins, pace_secs);
-
-    DistanceAndPace km_data, mile_data;
-    Time input_pace = {pace_mins, pace_secs};
-    convert_distance_and_pace(distance_unit, dist, pace, input_pace, km_data, mile_data);
-
-    print_results(date, time_run, km_data, mile_data);
+    Time run_duration = parse_run_time_input(run_time_input);
+    RunData run_data = RunData(distance, distance_unit, run_duration);
+    print_results(date, run_data);
 }
